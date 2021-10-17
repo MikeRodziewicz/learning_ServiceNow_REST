e5f9301ae74e65734f69b3f9503f5209a00adf6d
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-10-17 17:21:05 +0200

    restructure into util package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='snow_connector',
+    version='0.1',
+    description='utilities for connecting to ServiceNow',
+    url='https://github.com/MikeRodziewicz/snow_connector_pck',
+    author='MikeRodziewicz',
+    author_email='mike.python.testing@gmail.com',
+    license='unlicense',
+    packages=['snow_connector'],
+    zip_safe=False
+)
+

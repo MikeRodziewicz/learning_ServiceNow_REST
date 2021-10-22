9ba3f5d4df1a586cbc542dd0f06ef62e1b92f253
Mike.Rodziew <michal.rodziewicz1@gmail.com>
2021-10-22 21:50:01 +0200

    post incident func added and tested

--- snow_connector/connector.py
+++ snow_connector/connector.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,10 +18,9 @@ class MakeSnowConnection():
             'Accept': 'application/json',
         }
 
-    def _make_connection(self, method, url, **kwargs):
-        
+    def _make_connection(self, method, url, **kwargs): 
         try: 
-            self.response = requests.request(auth=(self.username, self.password), method=method, url=url, headers=self.headers, data=self.payload, **kwargs)
+            self.response = requests.request(auth=(self.username, self.password), method=method, url=url, headers=self.headers, json=self.payload, **kwargs)
             self.response.raise_for_status()
             self.response = self.response.json()
             return self.response
@@ -32,13 +32,24 @@ class MakeSnowConnection():
         url = f"{self.baseUrl}/api/now/table/incident?sysparm_query=number={inc_number}"
         return self._make_connection(method, url)
 
-
-base_url = os.getenv('BASE_URL')
-snow_usr = os.getenv('SNOW_USR')
-snow_pwd = os.getenv('SNOW_PWD')
-
-spam = MakeSnowConnection(base_url, snow_usr, snow_pwd)
-print(spam.get_single_incident('INC0000060'))
+    def post_single_incident(self, body):
+        method = "POST"
+        self.payload = body
+        url = f"{self.baseUrl}/api/now/table/incident"
+        return self._make_connection(method, url)
 
 
+if __name__ == "__main__":
+    
+    base_url = os.getenv('BASE_URL')
+    snow_usr = os.getenv('SNOW_USR')
+    snow_pwd = os.getenv('SNOW_PWD')
+    body = {
+        "urgency": "1",
+        "impact": "3",
+        "short_description": "test description"
+        }
 
+    spam = MakeSnowConnection(base_url, snow_usr, snow_pwd)
+
+    print(spam.post_single_incident(body))
